3a63301003f80534e11aad1df5bb959e862bf6a9
Nestor Aldana <nestor.aldana1@gmail.com>
2018-06-25 07:07:39 -0600

    Deleted field unused

--- src/com/naldana/data/Connection.java
+++ src/com/naldana/data/Connection.java
@@ -6,16 +6,12 @@ import java.sql.SQLException;
 public class Connection {
 
     private static Connection instance;
-    private static java.sql.Connection connection;
     private static final String password = "root";
     private static final String user = "root";
 
     private Connection() {
         try {
             Class.forName("org.mariadb.jdbc.Driver");
-            connection = getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
