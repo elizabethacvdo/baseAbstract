687dd3edee940391b91783f4ca257861f5503289
Nestor Aldana <nestor.aldana1@gmail.com>
2018-06-25 08:33:33 -0600

    Add pojo user

---
+++ src/com/naldana/data/entidades/Usuario.java
@@ -0,0 +1,30 @@
+package com.naldana.data.entidades;
+
+public class Usuario {
+    private int id;
+    private String user, password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
